4249f360ec7cdcef72c33cfd37947d6d82abe439
SiddhantSharma-0980 <siddhantsharma9582@gmail.com>
2026-05-18 19:50:07 +0530

    function start

---
+++ Experiment/Experiment 5/experiment5.jsx
@@ -0,0 +1,9 @@
+let employees = [];
+
+function addEmployee() {
+
+    let name = document.getElementById("name").value;
+    let id = document.getElementById("id").value;
+    let salary = parseFloat(document.getElementById("salary").value);
+    let department = document.getElementById("department").value;
+ 
