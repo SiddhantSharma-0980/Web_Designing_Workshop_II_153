ddf96aee8dde5d495a777fd7d483a33fd67a67ef
SiddhantSharma-0980 <siddhantsharma9582@gmail.com>
2026-05-18 20:23:03 +0530

    change heading function add

--- Experiment/Experiment 6/Experiment-6.jsx
+++ Experiment/Experiment 6/Experiment-6.jsx
@@ -2,3 +2,18 @@ function changeBackground(){
     
     document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 }
+function changeHeading(){
+
+    let name = document.getElementById("nameInput").value;
+
+    if (name==="") {
+        document.getElementById("heading").innerText =
+        "Welcome to JavaScript Lab";
+    }
+    
+    else{
+        document.getElementById("heading").innerText =
+        "Hello,"+name+"!";
+    }
+    
+}
