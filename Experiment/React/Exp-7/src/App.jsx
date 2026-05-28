1f4f77cfef2d2634b74e5cec30328f83e4b80213
SiddhantSharma-0980 <siddhantsharma9582@gmail.com>
2026-05-28 11:21:36 +0530

    Exp 7 start

---
+++ Experiment/React/Exp-7/src/App.jsx
@@ -0,0 +1,13 @@
+import Studentcard from "./studentcard.jsx";
+function App() {
+  return (
+    <div>
+      <h1>Welcome Cse 27 in react</h1>
+      <p>This is my first react app!</p>
+      <Studentcard name ="John Doe" age="20" course="Computer Science" marks="85" />
+    </div>
+  );
+
+}
+
+export default App;
